d57f3a6ba703f0960352b1cfa8acace4e6d37719
Robert <rlhead94@gmail.com>
2023-05-17 12:50:20 -0400

    hw

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
@@ -22,6 +23,14 @@ const handleErrors = (err) => {
   
     return errors;
   }
+// create json web token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'secret message', {
+    expiresIn: maxAge
+  });
+};
+
 
 module.exports.signup_get = (req, res) => {
     res.render('signup')
@@ -32,16 +41,20 @@ module.exports.login_get = (req, res) => {
 
 
 
-module.exports.signup_post = async(req, res) => {
-    const {email, password} = req.body;
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
-    const user = await  User.create({email, password});
-    res.status(201).json(user);
-    } catch (err) {
-        const errors = handleErrors(err);
-        res.status(400).json({ errors });
-    }
+  try {
+    const user = await User.create({ email, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
+  }
+  catch(err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+ 
 }
 module.exports.login_post = async(req, res) => {
     const {email, password} = req.body;
